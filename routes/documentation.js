32ed9ec9b8b53246c5626d73702f514db885eaf7
Piyush Patel <piyushpatelcodes@gmail.com>
2025-07-21 19:58:03 +0530

    resolved routing

--- routes/documentation.js
+++ routes/documentation.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const path = require('path');
 
 router.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '../views/documentation.html'));
+  res.sendFile(path.join(__dirname, '../views/index.html'));
 });
 
 module.exports = router;
